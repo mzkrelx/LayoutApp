5ad3effe99d520701b92c5f478512ea0734748b1
mzkrelx <mizuki@relx.jp>
2014-08-10 18:46:44 +0900

    ボタンとトースト

--- src/com/example/layoutapp/MainActivity.java
+++ src/com/example/layoutapp/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.layoutapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
-    @Override
+	Context context;
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
+		context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Touch‚³‚ê‚Ü‚µ‚½", Toast.LENGTH_SHORT).show();
+			}
+		});
     }
 
 
